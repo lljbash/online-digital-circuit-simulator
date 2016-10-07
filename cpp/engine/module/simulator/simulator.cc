062a44f3af787d2ff69930fe19144cdfd23099e7
Li Lingjie <llj_95@163.com>
2016-10-07 14:50:01 +0800

    add delay for test

--- cpp/engine/module/simulator/simulator.cc
+++ cpp/engine/module/simulator/simulator.cc
@@ -1,10 +1,12 @@
 #include "simulator.h"
+#include <unistd.h>
 
 using namespace psjjjj;
 using namespace std;
 
 SimulationResultProto Simulator::simulate(string vhdl_source_code) {
     PSJJJJ_SHOW("start simulating...");
+    usleep(5000000);
     //TODO: execute modelsim to simulate
     PSJJJJ_SHOW("end simulating.");
     
--- cpp/engine/module/vhdl_parser/vhdl_parser.cc
+++ cpp/engine/module/vhdl_parser/vhdl_parser.cc
@@ -1,10 +1,12 @@
 #include "vhdl_parser.h"
+#include <unistd.h>
 
 using namespace psjjjj;
 using namespace std;
 
 VHDLParsingResultProto VHDLParser::parse() {
     PSJJJJ_SHOW("parsing...");
+    usleep(5000000);
     PSJJJJ_SHOW("parsing done!");
     
     VHDLParsingResultProto vprp;
